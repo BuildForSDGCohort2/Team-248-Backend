cdc17e3e7d9f846058e9db3383814694facb2866
Amine TIYAL <aminetiyal@gmail.com>
2020-08-27 10:19:09 +0100

    update user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,7 +2,7 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\User;
+use App\Models\User;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 
@@ -22,7 +22,13 @@ $factory->define(User::class, function (Faker $faker) {
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'password' => 'password',
         'remember_token' => Str::random(10),
+        'phone_number' => $faker->phoneNumber,
+        'dob' => $faker->date(),
+        'profile_img' => $faker->imageUrl(),
+        'id_img' => $faker->imageUrl(),
+        'gender' => $faker->randomElement(['male','female']),
+        'is_active' => $faker->boolean,
     ];
 });
