ba1c01ca96051784d5826bc72271d5883dd7ec2e
Amine TIYAL <aminetiyal@gmail.com>
2020-08-27 10:17:53 +0100

    add AuthTest

---
+++ tests/Feature/Api/AuthTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Feature\Api;
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Laravel\Sanctum\Sanctum;
+use Tests\TestCase;
+
+class AuthTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /** @test */
+    public function user_can_login()
+    {
+        $user = factory('App\Models\User')->create();
+        $credentials = [
+            'email' => $user->email,
+            'password' => 'password'
+        ];
+
+        $this->post(route('api.login'), $credentials)->assertJsonStructure(['user', 'token']);
+    }
+
+    /** @test */
+    public function user_can_register()
+    {
+        $userData = factory('App\Models\User')->make()->toArray();
+        $password = [
+            'password' => 'password',
+            'password_confirmation' => 'password'
+        ];
+
+        $this->post(route('api.register'), array_merge($userData, $password))->assertJsonStructure(['user', 'token']);
+    }
+
+    /** @test */
+    public function user_can_logout()
+    {
+        $user = factory('App\Models\User')->create();
+        $credentials = [
+            'email' => $user->email,
+            'password' => 'password'
+        ];
+
+        Sanctum::actingAs($user, ['authToken']);
+
+        $this->post(route('api.logout'))->assertSee('Token deleted');
+    }
+}
