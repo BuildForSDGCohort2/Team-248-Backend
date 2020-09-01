39ca99b08acf8abf7f886778438046142b23eac9
Amine TIYAL <aminetiyal@gmail.com>
2020-09-01 10:18:36 +0100

    update auth routes

--- routes/api.php
+++ routes/api.php
@@ -13,8 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('register', 'Auth\RegisterController')->name('register');
+Route::post('login', 'Auth\LoginController')->name('login');
+Route::post('logout', 'Auth\LogoutController')->name('logout')->middleware('auth:sanctum');
+Route::get('user', 'Auth\UserController')->name('getUser')->middleware('auth:sanctum');
 
 Route::post('/offers', "OfferController@store");
