00875fe6132217e2cc913f10f38da16976ad6748
Amine TIYAL <aminetiyal@gmail.com>
2020-09-02 22:49:15 +0100

    add rollback support

--- database/migrations/2020_09_02_132413_update_users_table.php
+++ database/migrations/2020_09_02_132413_update_users_table.php
@@ -18,4 +18,17 @@ class UpdateUsersTable extends Migration
             $table->boolean('is_active')->default(true)->change();
         });
     }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('profile_img')->nullable(false)->change();
+            $table->boolean('is_active')->default(false)->change();
+        });
+    }
 }
