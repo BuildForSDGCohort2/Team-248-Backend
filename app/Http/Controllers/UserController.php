d0f8afe096ebabd8c2b7c5d2d52018a76a1057dc
Rana Emad <rana3emad@gmail.com>
2020-09-09 17:44:25 +0200

    add documentation comments for deactivate account

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,7 +8,24 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    
+    /**
+     * Deactivate a user account
+     *
+     * Enables the user to deactivate his account
+     * 
+     * @urlParam  user required The ID of the user to be deactivated.
+     * 
+     * @response  200 {
+     * "message": "Account deactivated successfully.",
+     * "data": ""
+     *}
+     *
+     *
+     * @response 401 {
+     *  "message": "Unauthenticated."
+     *}
+     *
+     */
     public function deactivate(Request $request, User $user, DeactivateAccountService $service)
     {
         return $service->execute($request, $user);
