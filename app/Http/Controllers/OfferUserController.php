e44624748d442537b8a67b41fff526ffaf831277
Nourhan <mnourrhan@gmail.com>
2020-09-29 13:38:28 +0200

    - solve conflict merge develop into view_applied_offers

--- app/Http/Controllers/OfferUserController.php
+++ app/Http/Controllers/OfferUserController.php
@@ -7,11 +7,13 @@ use App\Models\OfferUser;
 use App\Services\CancelOfferUserService;
 use App\Services\CreateOfferUserService;
 use Illuminate\Http\Request;
+use App\Http\Requests\RetrieveAppliedOffersRequest;
+use App\Services\RetrieveAppliedOffersService;
 
 class OfferUserController extends Controller
 {
 
-	/**
+    /**
      * @param RetrieveAppliedOffersRequest $request
      * @param RetrieveAppliedOffersService $service
      * @return \App\Http\Resources\ErrorResource|\App\Http\Resources\SuccessResource
